7f3a67b69569c4494296d133e780ca1ca9867c1a
Sophie Baxter <sbaxter95@outlook.com>
2019-04-21 19:30:05 +0100

    setup firebase and database

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
 const express = require('express');
 const app = express();
 const port = 3001;
+const axios = require('axios');
+
+const firebase = require('firebase/app');
+require('firebase/auth');
+require('firebase/database');
+
+const config = require('./config/firebase');
+
+firebase.initializeApp(config);
+
+const getDocuments = () => {
+  try {
+    return axios.get('https://scribe-aac31.firebaseio.com/documents.json');
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 app.get('/', (req, res) => {
   res.send('hello world');
 });
 
-app.listen(port, err => {
-  if (err) {
-    return console.log('something bad happened', err);
-  }
-
-  console.log(`server is listening on ${port}`);
+app.get('/user/documents', (req, res) => {
+  getDocuments().then(response => {
+    res.send(response.data);
+  });
 });
+
+app.listen(port);
+
+module.exports = app;
--- test/backend.test.js
+++ test/backend.test.js
@@ -1,68 +1,87 @@
+const app = require('../index.js');
+const request = require('supertest');
+const assert = require('assert');
+const expect = require('chai').expect;
+
+const mockResponse = '{ "name":"test" }';
+
 describe('backend functionality', () => {
+  it('should return OK status', function() {
+    return request(app)
+      .get('/')
+      .then(response => {
+        assert.equal(response.status, 200);
+      });
+  });
   it('should allow user to fetch their documents', () => {
-    expect(true).toBe(false);
+    return request(app)
+      .get('/user/documents')
+      .then(response => {
+        assert.equal(response.status, 200);
+        assert.equal(response.text, mockResponse);
+      });
   });
   it('should allow user to upload file to document', () => {
-    expect(true).toBe(false);
+    assert.equal(true, false);
   });
   it('should allow user to upload file to new document', () => {
-    expect(true).toBe(false);
+    assert.equal(true, false);
   });
   it('should should return the correct structure of data for each document', () => {
-    expect(true).toBe(false);
+    assert.equal(true, false);
   });
   it('should should return the difference between the file uploaded and the previous version', () => {
-    expect(true).toBe(false);
+    assert.equal(true, false);
   });
   it('should should show conflicts in a document', () => {
-    expect(true).toBe(false);
+    assert.equal(true, false);
   });
   it('should should allow the user to update a version', () => {
-    expect(true).toBe(false);
+    assert.equal(true, false);
   });
   it('should should allow the user to delete a version', () => {
-    expect(true).toBe(false);
+    assert.equal(true, false);
   });
   it('should should allow the user to read all versions of a document', () => {
-    expect(true).toBe(false);
+    assert.equal(true, false);
   });
   it('should should allow the user to give access to other users to the document', () => {
-    expect(true).toBe(false);
+    assert.equal(true, false);
   });
   it('should should allow the user to branch off from a specific version', () => {
-    expect(true).toBe(false);
+    assert.equal(true, false);
   });
   it('should should allow the user to merge two branches', () => {
-    expect(true).toBe(false);
+    assert.equal(true, false);
   });
   it('should should allow the user to get the latest version from any branch', () => {
-    expect(true).toBe(false);
+    assert.equal(true, false);
   });
   it('should should allow the user to make suggestions and comments when updating document', () => {
-    expect(true).toBe(false);
+    assert.equal(true, false);
   });
   it('should should allow the user to make an account with an email and password', () => {
-    expect(true).toBe(false);
+    assert.equal(true, false);
   });
   it('should should allow the user to add a profile picture', () => {
-    expect(true).toBe(false);
+    assert.equal(true, false);
   });
   it('should should allow the user to update their password', () => {
-    expect(true).toBe(false);
+    assert.equal(true, false);
   });
   it('should should allow the user to delete their account', () => {
-    expect(true).toBe(false);
+    assert.equal(true, false);
   });
   it('should should allow the user to delete documents', () => {
-    expect(true).toBe(false);
+    assert.equal(true, false);
   });
   it('should should allow the user to remove members from groups', () => {
-    expect(true).toBe(false);
+    assert.equal(true, false);
   });
   it('should should allow the user to add members to groups', () => {
-    expect(true).toBe(false);
+    assert.equal(true, false);
   });
   it('should should allow the user to specify levels of access to a document', () => {
-    expect(true).toBe(false);
+    assert.equal(true, false);
   });
 });
