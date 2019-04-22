e76802177b4a89f72e691c0b4350af46d5e7f721
Sophie Baxter <sbaxter95@outlook.com>
2019-04-22 16:29:19 +0100

    user can get associated documents returned from relevant endpoint

--- index.js
+++ index.js
@@ -9,7 +9,33 @@ require('firebase/database');
 
 const config = require('./config/firebase');
 
+let uid;
+
 firebase.initializeApp(config);
+
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // Can get user UID here
+    console.log('User is signed in');
+    uid = user.uid;
+  } else {
+    console.log('No user is signed in');
+    uid = uid;
+  }
+});
+
+const addDocument = (file, changes, branch) => {
+  try {
+    return axios.post('https://scribe-aac31.firebaseio.com/documents.json', {
+      uid: uid,
+      content: file,
+      changes: changes,
+      branch: branch
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 const getDocuments = () => {
   try {
@@ -19,14 +45,70 @@ const getDocuments = () => {
   }
 };
 
+const updateUsers = user => {
+  try {
+    return axios.post('https://scribe-aac31.firebaseio.com/users.json', user);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const register = (email, password) => {
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(email, password)
+    .then(user => {
+      // Create a new user in database
+      updateUsers(user);
+    })
+    .catch(function(error) {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.error(errorCode, errorMessage);
+    });
+};
+
 app.get('/', (req, res) => {
   res.send('hello world');
 });
 
 app.get('/user/documents', (req, res) => {
+  let result;
+  let docArr = [];
   getDocuments().then(response => {
-    res.send(response.data);
+    const properties = Object.values(response.data);
+    for (let i = 0; i < properties.length; i++) {
+      if (properties[i].uid === uid) {
+        docArr.push(properties[i]);
+        result = docArr;
+      } else {
+        result = 'No matching documents for this user';
+      }
+    }
+    res.send(result);
   });
+});
+
+app.get('/register', (req, res) => {
+  register('example@website.org', 'password');
+  res.send('Account created');
+});
+
+app.get('/login', (req, res) => {
+  firebase
+    .auth()
+    .signInWithEmailAndPassword('example@website.org', 'password')
+    .catch(function(error) {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.error(errorCode, errorMessage);
+    });
+  res.send('Logged in');
+});
+
+app.get('/document', (req, res) => {
+  addDocument('test', null, 'master');
+  res.send('added document');
 });
 
 app.listen(port);
